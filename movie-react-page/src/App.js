fcce4067e56616dca0e00b6a2636621bcfc69a5a
kim mari <53479112+mariHidream@users.noreply.github.com>
2022-09-08 20:46:42 +0900

    중간저장

--- movie-react-page/src/App.js
+++ movie-react-page/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if(toDo === "") return;
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo("");
+  }
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>My To Dos({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      
+      <input type="text" placeholder='Write your to do....' value={toDo} onChange={onChange}></input>
+      <button >Add To Do</button>
+    </form>
+    </>
   );
 }
 
