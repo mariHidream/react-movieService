dbebcba9315678226d3179f91e046ee0792e4c3c
kim mari <53479112+mariHidream@users.noreply.github.com>
2022-09-18 19:05:39 +0900

    page uproad

--- movie-react-page/src/components/Movie.js
+++ movie-react-page/src/components/Movie.js
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'; 
 
 const Movie = ({ item }) => {
-    const {medium_cover_image, title, summary, genres} = item
+    const {medium_cover_image, title, summary, genres, id } = item
     return (
         <div> 
             <img src={medium_cover_image} alt={title}/>
             <h2>
-                <Link to="/movie"> { title } </Link>
+                <Link to={`/movie/${id}`}> { title } </Link>
             </h2>
             <p>{summary}</p>
             <ul>
@@ -18,6 +18,7 @@ const Movie = ({ item }) => {
 };
 
 Movie.prototype = {
+    id : PropTypes.number.isRequired,
     medium_cover_image : PropTypes.string.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired, 
--- movie-react-page/src/routes/Detail.js
+++ movie-react-page/src/routes/Detail.js
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
+    const {id} = useParams();
+    const getMovies = async () => {
+        const json = await ( 
+            await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)
+        ).json();
+    }
+    useEffect(() => {
+        getMovies()
+    },[])
+
     return (
         <h1> detail </h1>
     );
